090c9c52773ea6f25e4328dc8a0184bafde11424
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-18 09:22:43 +0000

    typing

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,8 +2,9 @@
 """
 Module of 5-sum_list
 """
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """ sum of list of floats """
     return sum(input_list)
