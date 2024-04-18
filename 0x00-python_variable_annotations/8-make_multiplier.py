b6377a82bbee73da0c09304273b3a721c29cb549
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-18 09:51:51 +0000

    return callable

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -2,9 +2,9 @@
 """
 Module of 8-make_multiplier
 """
-from typing import Callable, Union
+from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    """  function that returns function """
-    return lambda multiplier: multiplier * 2
+    """ function that returns function """
+    return lambda multiplier: multiplier ** 2
