689855fdcff46caa6158786d42ceb5d90f33c4eb
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-22 09:17:29 +0000

    asynic IO hands on

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module of 0-basic_async_syntax
+"""
+
+import asyncio
+from random import uniform
+
+
+async def wait_random(max_delay=10):
+    """ async syntax """
+    delay = uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module of 1-concurrent_coroutines
+"""
+
+import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n, max_delay):
+    """ Multiple coroutines at the same time """
+    delays = []
+    tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
+    for t in asyncio.as_completed(tasks):
+        delay = await t
+        delays.append(delay)
+    return delays
