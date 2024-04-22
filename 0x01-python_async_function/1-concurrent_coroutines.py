747579f9c0639fbbf8aa6ad93a7a8ce9a470cc10
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-22 09:29:52 +0000

    asynic IO hands on

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,10 +4,11 @@ Module of 1-concurrent_coroutines
 """
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list:
+async def wait_n(n: int, max_delay: int) -> List:
     """ Multiple coroutines at the same time """
     delays = []
     tasks = [asyncio.create_task(wait_random(max_delay)) for i in range(n)]
