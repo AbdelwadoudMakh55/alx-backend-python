89756a3341251854bca745f2613a0dd5ab50fea8
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-22 09:21:39 +0000

    asynic IO hands on

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 """
 Module of 0-basic_async_syntax
+- Discovering basic async syntax
 """
 
 
 import asyncio
-from random import uniform
+import random
 
 
 async def wait_random(max_delay=10):
     """ async syntax """
-    delay = uniform(0, max_delay)
+    delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
