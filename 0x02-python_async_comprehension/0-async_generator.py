c59ac4363eb2a2c015f41e84dc75b7c54d9a60b1
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-04-23 12:25:56 +0000

    async generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,9 +4,10 @@ Module: 0-async_generator
 """
 import asyncio
 import random
+from typing import Iterator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Iterator[float]:
     """ async comprehension """
     for i in range(10):
         await asyncio.sleep(1)
