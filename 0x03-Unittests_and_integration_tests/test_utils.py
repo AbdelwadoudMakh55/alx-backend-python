0962b37a6bf1c25a590c97954f5c4bc2b79fba17
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-10 22:23:09 +0200

    Mock functions

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,8 +5,9 @@ Module for parameterized testing
 
 
 import unittest
+from unittest.mock import Mock, patch
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -31,3 +32,22 @@ class TestAccessNestedMap(unittest.TestCase):
         """ Function that test: access_nested_map in case of exception """
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Class for testing "utils.get_json"
+    """
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get_json):
+        """ Mock get_json """
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get_json.return_value = mock_response
+        self.assertEqual(get_json(test_url), test_payload)
+        mock_get_json.assert_called_once_with(test_url)
