56c5effb6f6fc477eb966d993a82ab390177295a
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-11 22:02:31 +0200

    More patching

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -44,11 +44,3 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(client.public_repos(), ["Ahmed", "Abd"])
             mock_get_json.assert_called_once()
             mock_public_repos.assert_called_once()
-
-    @parameterized.expand([
-        ({"license": {"key": "my_license"}}, "my_license", True),
-        ({"license": {"key": "other_license"}}, "my_license", False),
-    ])
-    def test_has_license(self, repo, license_key, res):
-        """ Function that tests: GithubOrgClient.has_license """
-        self.assertEqual(GithubOrgClient.has_license(repo, license_key), res)
