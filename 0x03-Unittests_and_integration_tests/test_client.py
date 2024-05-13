3f4e369370f33da0cbe28eb8fd52ab8248d9e5a0
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-05-13 12:18:31 +0200

    Fix

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Unittesting of "client.py" using patchers and parameterized
 """
@@ -14,16 +14,14 @@ class TestGithubOrgClient(unittest.TestCase):
     """
     Testing the GithubOrgClient class
     """
-
     @parameterized.expand([
         ("google",),
         ("abc",),
     ])
-    @patch("client.get_json")
+    @patch("client.get_json", return_value={"name": None})
     def test_org(self, org, mock_get_json):
         """ Test test_org """
         client = GithubOrgClient(org)
         url = GithubOrgClient.ORG_URL.format(org=org)
-        mock_get_json.return_value = {"name": None}
         self.assertEqual(client.org, {"name": None})
         mock_get_json.assert_called_once_with(url)
